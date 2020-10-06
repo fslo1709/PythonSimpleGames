87a8cfc6c2fdcaba7e2c90bd60d1b4554b5a00bf
Sebastian Lopez <sebastil@hotmail.com>
2020-10-06 21:18:46 +0800

    change of directories

---
+++ minigames/reaction.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 500
+SCREEN_POSITION = 50
+BG_COLOR = "black"
+TXT_COLOR1 = "white"
+FONT = "Helvetica"
+LBL_SIZE = 20
+
+class main(tk.Tk):
+	def __init__(self):
+		tk.Tk.__init__(self)
